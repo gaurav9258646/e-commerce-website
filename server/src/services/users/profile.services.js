33fa0c89b85ef8d29219725508d5b581cfa9ee54
Gaurav <you@example.com>
2025-08-28 19:47:01 +0530

    feat: git  images of products

--- server/src/services/users/profile.services.js
+++ server/src/services/users/profile.services.js
@@ -2,15 +2,53 @@
 const Cart = require("../../models/cart");
 const User= require("../../models/user");
 const WishList = require("../../models/wishlist");
+const Image = require("../../models/images");
 const profileUser=async(id)=>{
      const user = await User.findById(id).select("-password -__v");
 
   // get cart and wishlist
-  const cart = await Cart.find({ user: user._id });
-  const wishlist = await WishList.find({ user: user._id });
+const cart = await Cart.find({ user: user._id }).populate({
+    path: "item",
+    select: "title slug",
+  });
+
+  for (let c of cart) {
+    const images = await Image.find({ product_id: c.item._id }).limit(1);
+    if (images.length === 0) {
+      c.item = {
+        ...c.item._doc,
+        images: [{ url: "https://www.svgrepo.com/show/508699/landscape-placeholder.svg" }],
+      };
+      continue;
+    }
+    c.item = {
+      ...c.item._doc,
+      images: images,
+    };
+  }
+
+  const wishlist = await WishList.find({ user: user._id }).populate({
+    path: "item",
+    select: "title slug",
+  });
+
+  for (let w of wishlist) {
+    const images = await Image.find({ product_id: w.item._id }).limit(1);
+    if (images.length === 0) {
+      w.item = {
+        ...w.item._doc,
+        images: [{ url: "https://www.svgrepo.com/show/508699/landscape-placeholder.svg" }],
+      };
+      continue;
+    }
+    w.item = {
+      ...w.item._doc,
+      images: images,
+    };
+  }
 
   return { user, cart, wishlist };
     // fetch addresses with user id
     return user;
 };
-module.exports={profileUser}
+module.exports={profileUser} 
